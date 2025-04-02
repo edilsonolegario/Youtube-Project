ea23e5a1f82d11329116c0e7355c206192fd79c7
edilsonolegario <edilson_olegario@hotmail.com>
2025-04-01 21:44:02 -0300

    Deploying on April 1st

--- disney-clone/src/assets/Components/Header.jsx
+++ disney-clone/src/assets/Components/Header.jsx
@@ -1,8 +1,55 @@
 import React from 'react'
+import logo from '../images/logo.svg'
+
+import {
+  HiHome,
+  HiMagnifyingGlass,
+
+  HiStar,
+  HiPlayCircle,
+  HiTv
+} from 'react-icons/hi2';
+import { HiPlus, HiDotsVertical } from 'react-icons/hi';
+import HeaderItem from './HeaderItem';
 
 function Header() {
+  const menu = [
+    {
+      name: 'HOME',
+      icon: HiHome
+    },
+    {
+      name: 'SEARCH',
+      icon: HiMagnifyingGlass
+    },
+    {
+      name: 'WATCHLIST',
+      icon: HiPlus
+    },
+    {
+      name: 'ORIGINALS',
+      icon: HiStar
+    },
+    {
+      name: 'MOVIES',
+      icon: HiPlayCircle
+    },
+    {
+      name: 'SERIES',
+      icon: HiTv
+    }
+  ]
   return (
-    <div>Header</div>
+    <div className='flex items-center gap-8'>
+      <div className='flex'>
+        <img src={logo} className="w-[80px] md:w-[115px] object-cover"/>
+        {menu.map((item) => (
+          <HeaderItem name={item.name} Icon={item.icon} />
+        ))}
+      </div>
+      <img src="https://img.freepik.com/premium-vector/avatar-icon0002_750950-43.jpg?semt=ais_hybrid" 
+      class name="w-[10px] h-[10px] rounded-full object-cover"></img>
+    </div>
   )
 }
 
