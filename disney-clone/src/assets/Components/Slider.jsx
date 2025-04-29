f74f61a3c4c5bf29a6166965cf4ab36a682bc4c0
edilsonolegario <edilson_olegario@hotmail.com>
2025-04-29 15:53:21 -0300

    Deploying on April 29th

--- disney-clone/src/assets/Components/Slider.jsx
+++ disney-clone/src/assets/Components/Slider.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef, useState} from 'react'
 import GlobalAPI from '../../Services/GlobalAPI'
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi2';
 const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/original/'
 
 function Slider() {
-const [movieList, setMovieList] = React.useState([]);
-
+const [movieList, setMovieList] = useState([]);
+const elementRef = useRef();
     useEffect(() =>{
         getTrendingMovies(); 
     }, [])
@@ -15,19 +16,31 @@ const [movieList, setMovieList] = React.useState([]);
             setMovieList(resp.data.results);
         })
     }
+
+    const sliderRight = (element) => {
+        element.scrollLeft+=800
+    }
+    const sliderLeft = (element) => {
+        element.scrollLeft-=800
+    }
   return (
-    <div className="relative w-full px-16 py-4">
+    <div>
+      <HiChevronLeft className="hidden md:block text-white text-[30px] absolute mx-8 mt-[155px] cursor-pointer" onClick={() => sliderLeft(elementRef.current)}/>
+      <HiChevronRight className="hidden md:block text-white text-[30px] absolute mx-8 mt-[155px] cursor-pointer right-0" onClick={() => sliderRight(elementRef.current)}/>
+    <div className="relative w-full px-16 py-4" >
     <div className="flex overflow-x-auto w-full scrollbar-hide 
     hide-scrollbar no-scrollbar" 
+    ref={elementRef} 
     style={{
       scrollbarWidth: 'none',
       msOverflowStyle: 'none'
     }}>
-        {movieList.map((item, index) => (
+        {movieList.map((item) => (
             <img src={IMAGE_BASE_URL+item.backdrop_path} 
-            className='min-w-full h-[310px] object-cover
+            className='min-w-full md:h-[310px] object-cover
             object-left-top mr-5 rounded-md'/>
         ))}
+    </div>
     </div>
     </div>
   )
