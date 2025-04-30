3a92add9b5a145dc1c7440a7b32f25ad064d0801
edilsonolegario <edilson_olegario@hotmail.com>
2025-04-30 08:11:16 -0300

    Deploying on April 30th

--- disney-clone/src/App.jsx
+++ disney-clone/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Header from './assets/Components/Header'
 import Slider from './assets/Components/Slider'
+import ProductionHouse from './assets/Components/ProductionHouse'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
       <div className=''>
         <Header/>
         <Slider/>
+        <ProductionHouse/>
       </div>
   )
 }
--- disney-clone/src/assets/Components/Slider.jsx
+++ disney-clone/src/assets/Components/Slider.jsx
@@ -1,49 +1,71 @@
-import React, { useEffect, useRef, useState} from 'react'
-import GlobalAPI from '../../Services/GlobalAPI'
+import React, { useEffect, useRef, useState } from 'react';
+import GlobalAPI from '../../Services/GlobalAPI';
 import { HiChevronLeft, HiChevronRight } from 'react-icons/hi2';
-const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/original/'
+
+const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/original/';
+const screenWidth = window.innerWidth;
 
 function Slider() {
-const [movieList, setMovieList] = useState([]);
-const elementRef = useRef();
-    useEffect(() =>{
-        getTrendingMovies(); 
-    }, [])
+  const [movieList, setMovieList] = useState([]);
+  const elementRef = useRef();
 
-    const getTrendingMovies = () => {
-       GlobalAPI.getTrendingVideos().then(resp=>{
-            console.log(resp.data.results);
-            setMovieList(resp.data.results);
-        })
-    }
+  useEffect(() => {
+    getTrendingMovies();
+  }, []);
 
-    const sliderRight = (element) => {
-        element.scrollLeft+=800
+  const getTrendingMovies = () => {
+    GlobalAPI.getTrendingVideos().then((resp) => {
+      console.log(resp.data.results);
+      setMovieList(resp.data.results);
+    });
+  };
+
+  const sliderRight = () => {
+    if (elementRef.current) {
+      elementRef.current.scrollLeft += screenWidth-110;
     }
-    const sliderLeft = (element) => {
-        element.scrollLeft-=800
+  };
+
+  const sliderLeft = () => {
+    if (elementRef.current) {
+      elementRef.current.scrollLeft -= screenWidth-110;
     }
+  };
+
   return (
-    <div>
-      <HiChevronLeft className="hidden md:block text-white text-[30px] absolute mx-8 mt-[155px] cursor-pointer" onClick={() => sliderLeft(elementRef.current)}/>
-      <HiChevronRight className="hidden md:block text-white text-[30px] absolute mx-8 mt-[155px] cursor-pointer right-0" onClick={() => sliderRight(elementRef.current)}/>
-    <div className="relative w-full px-16 py-4" >
-    <div className="flex overflow-x-auto w-full scrollbar-hide 
-    hide-scrollbar no-scrollbar" 
-    ref={elementRef} 
-    style={{
-      scrollbarWidth: 'none',
-      msOverflowStyle: 'none'
-    }}>
+    <div className="relative w-full px-16 py-4">
+      {/* Slider Buttons */}
+      <HiChevronLeft
+        className="hidden md:block text-white text-[30px] absolute left-8 top-1/2 transform -translate-y-1/2 cursor-pointer z-10"
+        onClick={sliderLeft}
+      />
+      <HiChevronRight
+        className="hidden md:block text-white text-[30px] absolute right-8 top-1/2 transform -translate-y-1/2 cursor-pointer z-10"
+        onClick={sliderRight}
+      />
+
+      {/* Scrollable Content */}
+      <div
+        className="flex overflow-x-auto w-full scrollbar-hide"
+        ref={elementRef}
+        style={{
+          scrollbarWidth: 'none',
+          msOverflowStyle: 'none',
+        }}
+      >
         {movieList.map((item) => (
-            <img src={IMAGE_BASE_URL+item.backdrop_path} 
-            className='min-w-full md:h-[310px] object-cover
-            object-left-top mr-5 rounded-md'/>
+          item.backdrop_path && (
+            <img
+              key={item.id}
+              src={IMAGE_BASE_URL + item.backdrop_path}
+              className="min-w-full md:h-[310px] object-cover object-left-top mr-5 rounded-md hover:border-[4px] border-gray-400 transition-all duration-100 ease-in-out"
+              alt={item.title || 'Movie'}
+            />
+          )
         ))}
+      </div>
     </div>
-    </div>
-    </div>
-  )
+  );
 }
 
-export default Slider
+export default Slider;
